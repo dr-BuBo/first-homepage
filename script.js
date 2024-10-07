7a31d20dfbb1edd592fdc942972920755b7b8880
dr-BuBo <botond.buttyan@gmail.com>
2024-10-07 16:25:42 +0200

    final stage

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const toggle = document.getElementById("toggle");
 const nav = document.getElementById("nav");
 
 toggle.addEventListener("click", () => nav.classList.toggle("active"));
+
+fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then(data => {
+                const user = data.results[0];
+                const userPicture = document.getElementById('api');
+                userPicture.src = user.picture.large;
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+            });
